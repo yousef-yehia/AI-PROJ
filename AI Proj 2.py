c797379e48da8c846ff428720d25fc0d7d55cb18
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2023-05-19 00:59:15 +0300

    create board and evaluate_position

--- AI Proj 2.py
+++ AI Proj 2.py
@@ -128,3 +128,122 @@ while not selected_difficulty:
 pygame.time.wait(1000) # wait 1s to go to start the game
 difficulty = difficulty_levels[selected_difficulty]
 print(difficulty)
+
+
+
+def create_board():  # create_board
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))  # Define the rows and columns
+    return board
+
+def get_empty_row(board, col):   # get all empty rows at special column
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def is_empty(board, col): # column is empty or not
+    return board[ROW_COUNT - 1][col] == 0
+
+
+def empty_cols(board):  # array of empty columns
+    empty_cols = []
+    for col in range(COLUMN_COUNT):
+        if is_empty(board, col): # call function is_empty to know if the col empty or not
+            empty_cols.append(col) # add the col to array
+    return empty_cols
+
+
+
+def winning_check(board, piece):  # check if there 4 pieces horizontal or vertical or positively or negatively diaganols
+
+    # horizontal
+    for i in range(COLUMN_COUNT - 3): # if column_count = 3 => i = 0      the range [3,9]
+        for j in range(ROW_COUNT):  # doesn't matter value of row because [ horizontal ]
+            if board[j][i] == piece and board[j][i + 1] == piece and board[j][i + 2] == piece and \
+               board[j][i + 3] == piece: # the function will ckeck if col 0,1,2 and 3 same color AI will win
+               return True
+
+    # vertical
+    for i in range(COLUMN_COUNT):  # doesn't matter value of column because [ vertical ]
+        for j in range(ROW_COUNT - 3):  # if row_count = 3 => j = 0  the range [3,8]
+            if board[j][i] == piece and board[j + 1][i] == piece and board[j + 2][i] == piece and\
+               board[j + 3][ i] == piece: # the function will ckeck if row 0,1,2 and 3 same color AI will win
+               return True
+
+    # positively diaganols
+    for i in range(COLUMN_COUNT - 3): # if column_count = 3 => i = 0      the range [3,9]
+        for j in range(ROW_COUNT - 3): # if row_count = 3 => j = 0  the range [3,8]
+            if board[j][i] == piece and board[j + 1][i + 1] == piece and board[j + 2][i + 2] == piece and\
+               board[j + 3][i + 3] == piece: # the function will ckeck if row and col [0,0],[1,1],[2,2] and [3,3] same color AI will win
+               return True
+
+    # negatively diaganols   // there is wrong syntax bcs the col should decrease and the row should increase
+    for i in range(COLUMN_COUNT - 3):       # if column_count = 3 => i = 0                        the range [3,9]
+        for j in range(3, ROW_COUNT):       # the row_count will start by value 3  =>  j = 3      the range [3,5]
+            if board[j][i] == piece and board[j - 1][i + 1] == piece and board[j - 2][i + 2] == piece and \
+               board[j - 3][i + 3] == piece: # the function will ckeck if row and col [3,0],[2,1],[1,2] and [0,3] same color AI will win
+               return True
+
+
+
+def is_terminal(board):         #  send to function winning_check to know if AI1 or AI2  win or not
+    return winning_check(board, AI1_PIECE) or winning_check(board, AI2_PIECE) or len(empty_cols(board)) == 0
+
+
+
+def evaluate_position(board, piece):
+    score = 0
+
+    def evaluate_line(line):   # calculate score
+        opponent = 3 - piece   # Player 1 -> opponent 2, player 2 -> opponent 1
+        num_player_pieces = np.count_nonzero(line == piece)
+        num_opponent_pieces = np.count_nonzero(line == opponent)
+        if num_player_pieces == 4:  # if i am player 1 and num_of_pieces = 4 the score will increase 1000
+            score = 1000
+        elif num_player_pieces == 3 and num_opponent_pieces == 0: #  num_of_pieces = 3 the score will increase 100
+            score = 100
+        elif num_player_pieces == 2 and num_opponent_pieces == 0: #  num_of_pieces = 2 the score will increase 10
+            score = 10
+        elif num_player_pieces == 1 and num_opponent_pieces == 0: #  num_of_pieces = 1 the score will increase 1
+            score = 1
+        elif num_opponent_pieces == 4: # if opponent player num_of_pieces = 4 my score will decrease 1000
+            score = -1000
+        elif num_opponent_pieces == 3 and num_player_pieces == 0: # num_of_pieces = 3 my score will decrease 100
+            score = -100
+        elif num_opponent_pieces == 2 and num_player_pieces == 0: # num_of_pieces = 2 my score will decrease 10
+            score = -10
+        elif num_opponent_pieces == 1 and num_player_pieces == 0: # num_of_pieces = 1 my score will decrease 1
+            score = -1
+        else:
+            score = 0
+        return score
+        pass
+
+    #Score center column
+    center_count = sum(board[r][COLUMN_COUNT // 2] == piece for r in range(ROW_COUNT))
+    score += center_count * 3
+
+    # Evaluate columns
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            line = [board[r + i][c] for i in range(4)]
+            score += evaluate_line(line)
+
+    # Evaluate rows
+    for r in range(ROW_COUNT):
+        for c in range(COLUMN_COUNT - 3):
+            line = board[r][c:c + 4]
+            score += evaluate_line(line)
+
+    # Evaluate positive diagonals
+    for r in range(ROW_COUNT - 3):
+        for c in range(COLUMN_COUNT - 3):
+            line = [board[r + i][c + i] for i in range(4)]
+            score += evaluate_line(line)
+
+    # Evaluate negative diagonals
+    for r in range(ROW_COUNT - 3):
+        for c in range(COLUMN_COUNT - 3):
+            line = [board[r + i][c + 3 - i] for i in range(4)]
+            score += evaluate_line(line)
+
+        return score
